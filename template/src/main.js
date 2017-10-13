84d2db883d3992a9eb4568775598125e8f4c4a6f
jierry007 <jierry@gmail.com>
2017-10-13 11:15:42 +0800

    配置axios，vuex

--- template/src/main.js
+++ template/src/main.js
@@ -7,20 +7,30 @@ import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
+{{#vuex}}
+import store from './store'
+{{/vuex}}
+{{#axios}}
+import axios from 'axios'
+Vue.http = Vue.prototype.$http = axios
+{{/axios}}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  {{#router}}
-  router,
+{{#router}}
+router,
   {{/router}}
-  {{#if_eq build "runtime"}}
-  render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  template: '<App/>',
+{{#vuex}}
+store,
+  {{/vuex}}
+{{#if_eq build "runtime"}}
+render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+{{/if_eq}}
+{{#if_eq build "standalone"}}
+template: '<App/>',
   components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
+{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
