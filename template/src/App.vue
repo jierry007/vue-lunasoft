367a8f93dfb2f0062828d64b5904d2c53bfc64d9
jierry007 <jierry@gmail.com>
2017-10-13 11:16:49 +0800

    页面微调

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     {{#router}}
     <router-view/>
     {{else}}
-    <HelloWorld/>
+    <hello-world/>
     {{/router}}
   </div>
 </template>
@@ -12,7 +11,6 @@
 <script>
 {{#unless router}}
 import HelloWorld from './components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-
 {{/unless}}
 export default {
   name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
@@ -23,12 +21,46 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    font-size: 14px;
+    width: 100%;
+  }
+  html, body {
+    min-width: 1024px;
+    width: 100%;
+  }
+  
+  /* 重置文本格式元素 */
+  a { text-decoration: none; }
+  
+  /* 清除内外边距 */
+  body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, /* structural elements 结构元素 */
+  dl, dt, dd, ul, ol, li, /* list elements 列表元素 */
+  pre, /* text formatting elements 文本格式元素 */
+  fieldset, lengend, button, input, textarea, /* form elements 表单元素 */
+  th, td { /* table elements 表格元素 */
+    margin: 0;
+    padding: 0;
+  }
+  
+  /* 重置表单元素 */
+  legend { color: #000; } /* for ie6 */
+  fieldset, img { border: none; } /* img 搭车：让链接里的 img 无边框 */
+  /* 注：optgroup 无法扶正 */
+  button, input, select, textarea {
+    font-size: 100%; /* 使得表单元素在 ie 下能继承字体大小 */
+  }
+  
+  /* 重置表格元素 */
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  
+  /* 重置列表元素 */
+  ul, ol { list-style: none; }
 </style>
