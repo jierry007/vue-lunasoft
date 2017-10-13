d0cbd692a222b137e0a76915fd754dce3f83b7d5
jierry@gmail.com <jierry@gmail.com>
2017-10-13 11:30:52 +0800

    更新路由模版，添加less支持

---
+++ template/src/page/home/home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="page-home">
+      <hello></hello>
+      <p>
+        <a href="https://github.com/vuejs-templates" target="_blank">built yourself template , click it</a>
+      </p>
+    </div>
+</template>
+
+<script>
+    import Hello from '../../components/HelloWorld.vue'
+    export default {
+        name: 'page-home',
+        data() {
+            return {}
+        },
+        filters: {},
+        methods: {},
+      watch: {
+        /**
+         * 一个监测路由的 request
+         */
+        '$route': function () {
+          console.log(this.$route)
+            this.$http.get('/api/url', {
+              params: {
+                id: 'params'
+              }
+            })
+            .then( res => {
+                console.log(res)
+            })
+            .catch( err => {
+              console.log(err)
+            });
+        }
+      },
+        components: {
+          Hello
+        },
+        mounted: function () {
+        },
+    }
+</script>
+
+<style lang="less">
+   @import "home";
+</style>
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Router from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import HelloWorld from '@/components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+import HomePage from '../page/home/home.vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
+      path: '*',
+      redirect: '/'
+    },
+    {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      name: 'Home',
+      meta: {
+        requireAuth: false,
+      },
+      component: HomePage{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
     }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   ]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+/**
+ * 一个路由钩子，可以验证是否登陆
+ */
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    if (localStorage.getItem('username') !== null) {
+      next();
+    } else {
+      alert('please login')
+      next({
+        path: '/'
+      })
+    }
+  } else {
+    next();
+  }
+});
+
+export default router
